a7c3ee8a19b7dc062ee0f1f19dececc90745f3ea
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-15 08:03:24 -0400

    Install Voidfox built-in WebExtensions at startup

---
+++ overlay/mobile/android/fenix/app/src/main/java/org/mozilla/fenix/voidfox/VoidfoxBuiltInExtensions.kt
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.voidfox
+
+import android.util.Log
+import org.mozilla.fenix.FenixApplication
+
+/** Installs WebExtensions that ship as part of Voidfox itself. */
+object VoidfoxBuiltInExtensions {
+    private const val TAG = "VoidfoxBuiltIns"
+    private const val GX_ARCHIVE_EXTENSION_ID = "gx-archive-downloader@voidfox"
+    private const val GX_ARCHIVE_EXTENSION_URI =
+        "resource://android/assets/extensions/gx-archive-downloader/"
+
+    fun install(application: FenixApplication) {
+        application.components.core.geckoRuntime.webExtensionController
+            .ensureBuiltIn(GX_ARCHIVE_EXTENSION_URI, GX_ARCHIVE_EXTENSION_ID)
+            .accept(
+                { extension ->
+                    Log.i(TAG, "Built-in extension ready: ${extension.id}")
+                },
+                { error ->
+                    Log.e(TAG, "Unable to install the GX archive downloader", error)
+                },
+            )
+    }
+}
